59d434ad84105ef79c7054eb204cdd399c8b6b76
awan <aoneholics@gmail.com>
2023-12-25 20:13:07 +0700

    update #3

--- app/api/movies/[id]/route.js
+++ app/api/movies/[id]/route.js
@@ -4,15 +4,19 @@ import { NextResponse } from "next/server";
 
 export async function GET(request, {params}){
     const type = params.id;
-    // await connectMongoDB();
-    // const movie = await Movie.find({type: type});
-    // return NextResponse.json({movie}, {status: 200});
-    const movies = await fetch('https://doodapi.com/api/folder/list?key=339596z2p4etizfcyrqvj6&fld_id='+`${type}`, {
-        cache: "force-cache",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-    })
-    const doodmovies = await movies.json();
-    return NextResponse.json({doodmovies});
+    if((type=="movie") || (type=="recent")){
+        await connectMongoDB();
+        const movie = await Movie.find({type: type});
+        return NextResponse.json({movie}, {status: 200});
+    }
+    else{
+        const movies = await fetch('https://doodapi.com/api/folder/list?key=339596z2p4etizfcyrqvj6&fld_id='+`${type}`, {
+            cache: "force-cache",
+            headers: {
+              'Content-Type': 'application/json',
+            },
+        })
+        const doodmovies = await movies.json();
+        return NextResponse.json({doodmovies});
+    }    
 }
